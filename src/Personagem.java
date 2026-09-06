1b3c406621fb97ac096c72492c60a0bec48ba6c7
jhenrilima27-web <jhenrilima27@gmail.com>
2026-09-06 00:11:45 -0300

    feat: adiciona construtor em Personagem

--- src/Personagem.java
+++ src/Personagem.java
@@ -2,4 +2,10 @@ public class Personagem {
         String nome;
         int vida;
         int nivel;
+
+        public Personagem(String nome, int vida, int nivel) {
+                this.nome = nome;
+                this.vida = vida;
+                this.nivel = nivel;
+        }
 }
