61badd5f9874265ea46ced48cf73035bc35832ce
jhenrilima27-web <jhenrilima27@gmail.com>
2026-09-09 17:38:58 -0300

    feat: instancia primeiro objeto Personagem

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Personagem joiu = new Personagem("Joiu", 120, 55);
+
+        System.out.println(joiu.nome);
+        System.out.println(joiu.vida);
+        System.out.println(joiu.nivel);
     }
 }
