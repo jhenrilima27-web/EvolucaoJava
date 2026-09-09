c251b3f059bd069a5e267bbc01679cb0f795e9ea
jhenrilima27-web <jhenrilima27@gmail.com>
2026-09-09 17:50:57 -0300

    feat: adiciona construtor ao Guerreir

--- src/Guerreiro.java
+++ src/Guerreiro.java
@@ -1,2 +1,5 @@
 public class Guerreiro extends Personagem {
+        public Guerreiro(String nome, int vida, int nivel) {
+            super(nome, vida, nivel);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ public class Main {
     public static void main(String[] args) {
 
         Personagem joiu = new Personagem("Joiu", 120, 55);
+        Guerreiro arthur = new Guerreiro("Guerreiro", 150, 60);
 
-        System.out.println(joiu.nome);
-        System.out.println(joiu.vida);
-        System.out.println(joiu.nivel);
+        System.out.println(arthur.nome);
+        System.out.println(arthur.vida);
+        System.out.println(arthur.nivel);
 
-        joiu.atacar();
     }
 }
